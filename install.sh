10c6c02b375aa44205c5da1b8a1111a2ee2db3a5
Merculous <Merculous@users.noreply.github.com>
2019-12-22 07:10:27 -0800

    Now inserts xpwn include headers to /usr/local/include/xpwn

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ make clean
 make libXPwn.a common
 cp -rnv ipsw-patch/libxpwn.a /usr/local/lib
 cp -rnv common/libcommon.a /usr/local/lib
+# The modified headers just have the header paths fixed. Replaced "<>" with quotes.
+echo -e "Installing the modifided headers from includes to /usr/local/include for programs like libipatcher so they can find these headers"
+mkdir -p /usr/local/include/xpwn
+unzip -d /usr/local/include/xpwn xpwn-modified-headers.zip
