d5f40fc4b498e3122119a96bfa62bd3d6729bbc4
Merculous <Merculous@users.noreply.github.com>
2019-12-22 05:30:06 -0800

    Builds libxpwn.a and libcommon.a for MacOS

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.0 -DOPENSSL_LIBRARIES=/usr/local/opt/openssl@1.0/lib .
-make
-make libXPwn.a
+make clean
+make libXPwn.a common
+cp -rnv ipsw-patch/libxpwn.a /usr/local/lib
+cp -rnv common/libcommon.a /usr/local/lib
